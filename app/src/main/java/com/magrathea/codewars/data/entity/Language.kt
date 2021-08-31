cd948e2558c5431c96e7405b25a703250be61efe
gabrieltavares0123 <gabriel.tavaresramos@gmail.com>
2021-08-31 17:30:57 -0300

    refactor: Language to be an entity

--- app/src/main/java/com/magrathea/codewars/data/entity/Language.kt
+++ app/src/main/java/com/magrathea/codewars/data/entity/Language.kt
@@ -1,11 +1,10 @@
 package com.magrathea.codewars.data.entity
 
-import android.os.Parcelable
-import androidx.room.TypeConverters
-import kotlinx.parcelize.Parcelize
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Parcelize
+@Entity
 data class Language(
-    var name: String? = null,
+    @PrimaryKey var name: String? = null,
     var rank: Int? = 0,
-) : Parcelable
+)
