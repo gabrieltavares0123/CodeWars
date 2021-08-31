213d36d10a6dead94237bf0403b9f0f18867e3f2
gabrieltavares0123 <gabriel.tavaresramos@gmail.com>
2021-08-31 16:27:27 -0300

    fix: missing @InstallIn annotation in ApiModule and Databasemodule

--- app/src/main/java/com/magrathea/codewars/data/local/DatabaseModule.kt
+++ app/src/main/java/com/magrathea/codewars/data/local/DatabaseModule.kt
@@ -4,9 +4,12 @@ import android.app.Application
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 
 @Module
+@InstallIn(SingletonComponent::class)
 class DatabaseModule {
     companion object {
         private const val DATABASE_NAME = "codewars_database"
--- app/src/main/java/com/magrathea/codewars/data/remote/ApiModule.kt
+++ app/src/main/java/com/magrathea/codewars/data/remote/ApiModule.kt
@@ -2,10 +2,13 @@ package com.magrathea.codewars.data.remote
 
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
+@InstallIn(SingletonComponent::class)
 class ApiModule {
     companion object {
         private const val BASE_URL = "https://www.codewars.com/api/v1/"
