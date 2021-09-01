45587df710562acb351e8d7ddd743d4c8d9b8038
gabrieltavares0123 <gabriel.tavaresramos@gmail.com>
2021-09-01 01:01:11 -0300

    fix: remove unnecessary id from Language

--- app/src/main/java/com/magrathea/codewars/data/entity/Language.kt
+++ app/src/main/java/com/magrathea/codewars/data/entity/Language.kt
@@ -1,11 +1,9 @@
 package com.magrathea.codewars.data.entity
 
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 
 @Entity
 data class Language(
-    @PrimaryKey(autoGenerate = true) var languageId: Int,
     var languageName: String,
     var rank: Int? = 0,
 )
