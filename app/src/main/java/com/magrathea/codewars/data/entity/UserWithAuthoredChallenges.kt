24af6e709fd3c21dab8d4c3c7f0d6148824315f6
gabrieltavares0123 <gabriel.tavaresramos@gmail.com>
2021-08-31 23:26:47 -0300

    fix: change UserWithAuthoredChallenges to use the local model definitions

--- app/src/main/java/com/magrathea/codewars/data/entity/UserWithAuthoredChallenges.kt
+++ app/src/main/java/com/magrathea/codewars/data/entity/UserWithAuthoredChallenges.kt
@@ -4,7 +4,7 @@ import androidx.room.Embedded
 import androidx.room.Relation
 
 data class UserWithAuthoredChallenges(
-    @Embedded val user: User,
+    @Embedded val user: User.LocalUser,
     @Relation(parentColumn = "userId", entityColumn = "userAuthorId")
-    val authoredChallenges: List<AuthoredChallenge>
+    val authoredChallenges: List<AuthoredChallenge.LocalAuthoredChallenge>
 )
