09290407ff1360e21c54b1c420d32994411e703e
gabrieltavares0123 <gabriel.tavaresramos@gmail.com>
2021-09-02 17:15:20 -0300

    refactor: rename CompletedChallenge to AuthoredChallengeDetailsFragment fragment and view model

--- app/src/main/java/com/magrathea/codewars/presentation/challengedetais/AuthoredChallengeDetailsFragment.kt
+++ app/src/main/java/com/magrathea/codewars/presentation/challengedetais/AuthoredChallengeDetailsFragment.kt
@@ -2,5 +2,5 @@ package com.magrathea.codewars.presentation.challengedetais
 
 import androidx.fragment.app.Fragment
 
-class ChallengeDetailsFragment : Fragment() {
+class AuthoredChallengeDetailsFragment : Fragment() {
 }
--- app/src/main/java/com/magrathea/codewars/presentation/challengedetais/AuthoredChallengeDetailsViewModel.kt
+++ app/src/main/java/com/magrathea/codewars/presentation/challengedetais/AuthoredChallengeDetailsViewModel.kt
@@ -2,5 +2,5 @@ package com.magrathea.codewars.presentation.challengedetais
 
 import androidx.lifecycle.ViewModel
 
-class ChallengeDetailsViewModel : ViewModel() {
+class AuthoredChallengeDetailsViewModel : ViewModel() {
 }
