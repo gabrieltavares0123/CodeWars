840b3938685aba75b1b2bef61c3f7c5571bff021
gabrieltavares0123 <gabriel.tavaresramos@gmail.com>
2021-09-02 21:56:47 -0300

    chore: add basic structure for list member fragment

--- app/src/main/java/com/magrathea/codewars/presentation/listmembers/ListMembersFragment.kt
+++ app/src/main/java/com/magrathea/codewars/presentation/listmembers/ListMembersFragment.kt
@@ -1,6 +1,18 @@
 package com.magrathea.codewars.presentation.listmembers
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.magrathea.codewars.R
 
 class ListMembersFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list_members, container, false)
+    }
 }
