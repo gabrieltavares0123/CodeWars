17c7a5679935bc3e0dd9485546c8cfe235505804
gabrieltavares0123 <gabriel.tavaresramos@gmail.com>
2021-09-04 14:28:56 -0300

    chore: add Resource.kt class to be used in view models

---
+++ app/src/main/java/com/magrathea/codewars/util/Resource.kt
@@ -0,0 +1,9 @@
+package com.magrathea.codewars.util
+
+sealed class Resource<out T: Any> {
+    data class Success<out T: Any>(val data: T?): Resource<T>()
+    data class Error(val exception: Throwable): Resource<Nothing>()
+    object Loading: Resource<Nothing>()
+
+    fun toData(): T? = if(this is Success) this.data else null
+}
