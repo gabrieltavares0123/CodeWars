7319e4da2cebce98c5e78306eb5a4d19dfcdac49
gabrieltavares0123 <gabriel.tavaresramos@gmail.com>
2021-09-06 13:34:48 -0300

    refactor: remove possible nullable data in Resource for Success

--- app/src/main/java/com/magrathea/codewars/util/Resource.kt
+++ app/src/main/java/com/magrathea/codewars/util/Resource.kt
@@ -1,10 +1,12 @@
 package com.magrathea.codewars.util
 
 sealed class Resource<out T: Any> {
-    data class Success<out T: Any>(val data: T?): Resource<T>()
+    data class Success<out T: Any>(val data: T): Resource<T>()
     data class Error(val exception: Throwable): Resource<Nothing>()
     object Loading: Resource<Nothing>()
 
     fun toData(): T? = if(this is Success) this.data else null
     val isSuccess get() = this is Success
+    val isLoading get() = this is Loading
+    val isError get() = this is java.lang.Error
 }
